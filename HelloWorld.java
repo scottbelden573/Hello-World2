27f374754237ba1577a10d989038331d051febf8
scottbelden573 <67715406+scottbelden573@users.noreply.github.com>
2022-11-30 08:38:06 -0600

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+// HelloWorld.java
+
+public class HelloWorld {
+    // main method begins execution of Java application
+    public static void main(String[] args){
+      System.out.println("Hello World");
+    }
+}
